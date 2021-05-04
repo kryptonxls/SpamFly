43f49b3237a88d6d24ebe88db6d19254aedf6da6
kryptonxls <83668701+kryptonxls@users.noreply.github.com>
2021-05-04 19:07:35 +0000

    Add files via upload

---
+++ SpamFly.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import os
+os.system ("apt-get install figlet")
+os.system("clear")
+os.system("figlet Spam Fly")
+print("""
+
+YAPIMCI: KRYPTON.XLS
+
+SPAM FLY  
+
+YOUTUBE KANALIM:
+https://youtu.be/SItWrpbx7gc
+
+""")
+proxy = raw_input("proxy kullanmak istermisiniz Y/N:")
+print("""
+1: hesap sikayet et.
+2: bir video veya post sikayet et.
+""")
+secim = raw_input("sikayet yontemi seciniz:")
+
+if(secim == "1"):
+	hesap = raw_input("hesap adi: ")
+elif(secim == "2"):
+	post = raw_input("video veya post linki: ")
+else:
+	print("Boyle Bir Komut Yok")
+
+print("""
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+
+Hesap Sikayet Edildi!
+""")
